c741ae3483650214e6f6662a4077803ac2147e0a
cheeze2000 <me@cheeze2000.net>
2022-10-11 21:07:06 +0800

    refactor: try out greedy agent ai

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from Simulator import Simulator
-from Agent import HumanAgent, RandomAgent
+from Agent import *
 
 # Start a simulator that lets me play with an AI that chooses a random action.
 me = HumanAgent()
-ai = RandomAgent()
+ai = GreedyAgent()
 simulator = Simulator(me, ai)
 simulator.start()
