708f286ae8b5d11a2b8d238d6eb6a0098103eebc
cheeze2000 <me@cheeze2000.net>
2022-10-11 21:07:01 +0800

    feat: greedy agent

--- Agent.py
+++ Agent.py
@@ -15,3 +15,25 @@ class HumanAgent(Agent):
 	def policy(self, game):
 		move = input("Choose a house (0 to 5): ")
 		return int(move)
+
+# An agent that greedily chooses the best action every turn.
+class GreedyAgent(Agent):
+	def policy(self, game):
+		max_score = -1
+		best_action = -1
+
+		turn = game.turn
+		for move in range(6):
+			if turn == 'x' and game.board[move] == 0: continue;
+			if turn == 'y' and game.board[move + 7] == 0: continue;
+
+			next_game = game.move(move)
+			if next_game.turn == turn: return move
+
+			next_score = next_game.score(turn)
+			if next_score <= max_score: continue
+
+			max_score = next_score
+			best_action = move
+
+		return best_action
