d3d0ece5ce2953da05c75076a5ef233db15069ab
Mark Wong <markmcwong@yahoo.com>
2022-10-22 18:33:26 +0800

    Add: DeepQLearningAgent

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,3 +2,4 @@ from .GreedyAgent import GreedyAgent
 from .HumanAgent import HumanAgent
 from .MinimaxAgent import MinimaxAgent
 from .RandomAgent import RandomAgent
+from .DeepQLearningAgent import DeepQLearningAgent
